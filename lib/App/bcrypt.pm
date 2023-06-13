a2dd02adc2f13ae8096a20ef5458c8339f4dc6dd
brian d foy <briandfoy@pobox.com>
2023-06-13 01:01:06 -0400

    initial creation

---
+++ lib/App/bcrypt.pm
@@ -0,0 +1,65 @@
+package App::bcrypt;
+use strict;
+
+use warnings;
+no warnings;
+
+our $VERSION = '0.001_01';
+
+=encoding utf8
+
+=head1 NAME
+
+App::bcrypt - A command-line tool to deal with bcrypt password hashing
+
+=head1 SYNOPSIS
+
+	use App::bcrypt;
+
+=head1 DESCRIPTION
+
+=over 4
+
+=item new
+
+=cut
+
+sub new {
+
+	}
+
+=item init
+
+=cut
+
+sub init {
+
+	}
+
+=back
+
+=head1 TO DO
+
+
+=head1 SEE ALSO
+
+
+=head1 SOURCE AVAILABILITY
+
+This source is in Github:
+
+	http://github.com/briandfoy/app-bcrypt
+
+=head1 AUTHOR
+
+brian d foy, C<< <brian d foy> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2023, brian d foy, All Rights Reserved.
+
+You may redistribute this under the terms of the Artistic License 2.0.
+
+=cut
+
+1;
